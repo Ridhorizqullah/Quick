db2f49272a745ff32997a04a5c3e6a5280802952
Ridhorzqullah <ridhorizqullah3@gmail.com>
2024-03-29 09:31:40 +0700

    prosedur q_short

--- QuickSort_0131/QuickSort_0131.cpp
+++ QuickSort_0131/QuickSort_0131.cpp
@@ -39,3 +39,43 @@ void swap(int x, int y)
 	mov_count++;
 
 }
+
+void q_short(int low, int high)
+{
+	int temp;
+	int pivot, i, j;
+	if (low > high) {
+		return;
+	}
+
+	pivot = arr[low];
+	i = low + 1;
+	j = high;
+
+	while (i <= j)
+	{
+		while ((arr[i] <= pivot) && (i <= high))
+		{
+			i++;
+			cmp_count++;
+		}
+		cmp_count++;
+		while ((arr[j]) <= pivot && (i <= high))
+		{
+			j--;
+			cmp_count++;
+		}
+		cmp_count++;
+		if (i < j)
+		{
+			swap(i, j);
+		}
+	}
+	if (low < j) {
+		swap(low, j);
+	}
+
+	q_short(low, j - 1);
+	q_short(j + 1, high);
+}
+
