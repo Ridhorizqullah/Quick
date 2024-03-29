102ba18a17bc1b3f21e2c8bf1e12cb01b4f82049
Ridhorzqullah <ridhorizqullah3@gmail.com>
2024-03-29 09:02:12 +0700

    fungsi perintah

--- QuickSort_0131/QuickSort_0131.cpp
+++ QuickSort_0131/QuickSort_0131.cpp
@@ -1,10 +1,4 @@
-// QuickSort_0131.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+include < iostream>
+using namespace std;
 
-#include <iostream>
-
-int main()
-{
-    std::cout << "Hello World!\n";
-}
 
